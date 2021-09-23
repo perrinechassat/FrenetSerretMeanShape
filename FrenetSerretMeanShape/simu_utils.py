2ed7d56c6e77f3c4b2544627fc663e9f688407fb
Perrine Chassat <perrine.chassat@gmail.com>
2021-09-23 11:14:32 +0200

    fix code for spherical curves

--- FrenetSerretMeanShape/simu_utils.py
+++ FrenetSerretMeanShape/simu_utils.py
@@ -376,7 +376,7 @@ def pre_process_data_sphere(data, t_init, n_resamples, param_loc_poly_deriv, par
     k = 0
     param_TNB = param_loc_poly_TNB.copy()
     while success==False and k<10:
-        Q_LP, vkappa, Param, Param0, vparam, success = X.TNB_locPolyReg(grid_in=X.S(X.t), grid_out=new_grid_S, h=param_loc_poly_TNB['h'], p=param_loc_poly_TNB['p'], iflag=param_loc_poly_TNB['iflag'],
+        Q_LP, vkappa, Param, Param0, vparam, success = X.TNB_locPolyReg(grid_in=X.S(X.t), grid_out=new_grid_S, h=param_TNB['h'], p=param_TNB['p'], iflag=param_TNB['iflag'],
          ibound=param_loc_poly_TNB['ibound'], local=locpolyTNB_local)
         if locpolyTNB_local==False:
             param_TNB["h"]+=0.01
